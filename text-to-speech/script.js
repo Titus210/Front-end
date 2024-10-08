35bda84e868db0cf0612ed4d6f60efa85dc5f1b1
Titus210 <tituskiplagat210@gmail.com>
2024-10-08 22:04:49 +0300

    feat: implementation of speech Api to convert text to sound

--- text-to-speech/script.js
+++ text-to-speech/script.js
@@ -84,3 +84,56 @@ function createBox(item){
     main.appendChild(box);
 }
 
+// initialize speech syntesis
+const message = new SpeechSynthesisUtterance();
+
+// store voices
+let voices = [];
+
+function getVoices(){
+    voices = speechSynthesis.getVoices();
+
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+
+        voiceSelect.appendChild(option);
+    });
+}
+
+// set text
+function setTextMessage(text){
+    message.text = text;
+}
+
+// speak text
+function speakText(){
+    speechSynthesis.speak(message);
+}
+
+// set voice
+function setVoice(e){
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// voice changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// toggle text box
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
+
+// close button
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+
+// change voice
+voiceSelect.addEventListener('change', setVoice);
+
+// read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value);
+    speakText();
+});
+
+getVoices();
